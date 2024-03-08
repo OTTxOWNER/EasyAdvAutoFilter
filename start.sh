ab0844afdd90dfd01bf8c4be0591b0a6ffe75c5a
OTTxOWNER <162464699+OTTxOWNER@users.noreply.github.com>
2024-03-08 21:32:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-if [ -z https://github.com/Bhadvaheroku/EasyAdvAutoFilter.git ]
+if [ -z https://github.com/OTTxOWNER/EasyAdvAutoFilter.git ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bhadvaheroku/EasyAdvAutoFilter.git
+  git clone https://github.com/OTTxOWNER/EasyAdvAutoFilter.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/Bhadvaheroku/EasyAdvAutoFilter.git
+  git clone https://github.com/OTTxOWNER/EasyAdvAutoFilter.git
 fi
 cd EasyAdvAutoFilter
 pip3 install -U -r requirements.txt
